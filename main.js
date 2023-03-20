2faa7f84597e3b870dcfdf0f0af5a57432582a5c
Ramki29 <ramkie7010@gmail.com>
2023-03-20 21:50:49 +0530

    add odd-number

---
+++ main.js
@@ -0,0 +1,13 @@
+var oddNumber=parseInt(prompt("Enter the number:"));
+function odd(oddNumber)
+		{
+		if(oddNumber%2==1)
+			{
+			document.write("odd number");
+			}
+		else
+			{
+			document.write("even number");
+			}
+		}
+		odd(oddNumber);
